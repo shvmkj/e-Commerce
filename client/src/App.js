0e4c2f7dbf255ec33c7149d46118e3cc17fd8cd3
shvmkj <sheevam1101@gmail.com>
2021-08-29 08:43:30 +0530

    added User Login screen

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screen/HomeScreen";
 import ProductScreen from "./screen/ProductScreen";
 import CartScreen from "./screen/CartScreen";
+import LoginScreen from "./screen/LoginScreen";
 
 const App = ()=>{
   return (
@@ -18,6 +19,7 @@ const App = ()=>{
       </Route>
       <Route path='/product/:id' component={ProductScreen} />
       <Route path='/cart/:id?' exact component={CartScreen} />
+      <Route path='/login' exact component={LoginScreen} />
     </Container>
   </main>
   <Footer/>
