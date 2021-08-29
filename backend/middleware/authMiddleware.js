015db77e51c270c4465efc69d7256eecbf2dac0f
shvmkj <sheevam1101@gmail.com>
2021-08-29 07:14:29 +0530

    added user creation and authorization using jwt

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+import AsyncHandler from 'express-async-handler'
+const protect = AsyncHandler( async (req,res,next)=>{
+  let token = ''
+  if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+    try{
+      token = req.headers.authorization.split(' ')[1]
+      const decoded = jwt.verify(token,process.env.JWT_SECRET)
+    //  console.log(decoded.id,token)
+      req.user = await User.findById({_id:decoded.id}).select("-password")
+      console.log(JSON.stringify(req.user))
+      next()
+    }
+    catch(error){
+      console.log(error)
+      
+        res.status(401)
+        throw new Error('Not authorized ,Token Failed')
+    }
+  }else{
+    if(!token){
+      res.status(401)
+      throw new Error('Not authorized to perform this task')
+    }
+  }
+})
+
+export {protect}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,24 +1,10 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
+import { getProductById, getProducts } from '../controllers/productControllers.js'
 const router = express.Router()
 import Product from '../models/productModel.js'
-//Fetch All Products
-//@route GET /api/products
-//@access public 
-router.get('/',asyncHandler ( async (req,res)=>{
-  const products = await Product.find({})
-  res.json(products)
-}))
-//Fetch Single Product
-//@route GET /api/products/:id
-//@access public 
-router.get('/:id',asyncHandler ( async(req,res)=>{
-  const product = await Product.findById(req.params.id)
-  if(product){
-    res.json(product)
-  }else{
-    res.status(404)
-    throw new Error('Product Not Found')
-  }
-}))
+
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
+
 export default router
